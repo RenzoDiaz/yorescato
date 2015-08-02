1af276a5102254d7f95f5f2c3f3752877f628ae2
renzoMx <renzomx16@gmail.com>
2015-08-01 19:24:23 -0500

    without icons

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,7 @@ module Yorescato
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    config.assets.paths += Dir["#{Rails.root}/vendor/assets/*"].sort_by { |dir| -dir.size }
   end
 end
