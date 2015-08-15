eaa3de1c7922940ccb720657d62c1530f5d7b5ad
renzoMx <renzomx16@gmail.com>
2015-08-15 17:20:36 -0500

    Se agrego la pagina animals

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -1,4 +1,7 @@
 class LeadsController < ApplicationController
+	# Prevent CSRF attacks by raising an exception.
+	# For APIs, you may want to use :null_session instead.
+	protect_from_forgery with: :exception
 	def create 					
 		@lead = Lead.new lead_params
 		respond_to do |format|
