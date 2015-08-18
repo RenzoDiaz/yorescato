186230dcedc5902ae01c512870b33eb9f08459de
renzoMx <renzomx16@gmail.com>
2015-08-18 17:16:03 -0500

    Se modifico los estilos de lista de animales

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -3,6 +3,7 @@ class AnimalsController < ApplicationController
 	# For APIs, you may want to use :null_session instead.
 	protect_from_forgery with: :exception
 	def index
+		@navbar = true
 		@client_ip = remote_ip()
 	end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
 	# Prevent CSRF attacks by raising an exception.
 	# For APIs, you may want to use :null_session instead.
 	protect_from_forgery with: :exception
-	def index
+	def index		
 		@client_ip = remote_ip()
 	end
 end
